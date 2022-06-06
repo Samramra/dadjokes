9399b4c5a4482b4a3b609a91f27bc0592c272011
Šárka Mráčková <sarka7091@gmail.com>
2022-06-06 19:12:49 +0200

    úkol 15 pokračování vtipu

--- src/index.jsx
+++ src/index.jsx
@@ -2,41 +2,25 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
 import './style.css';
-
+import { Joke } from './Joke';
+import { jokes } from './jokes.js';
 
 const App = () => {
-  const [upLike, setUpLike] = useState(0);
-  const [downLike, setDownLike] = useState(0);
-
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p className="user-name">Neroxx</p>
-          </div>
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-
-        <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up" onClick={() => setUpLike(upLike + 1)}></button>
-          <span id="likes-up" className="likes-count likes-count--up">{upLike}</span>
-          <button id="btn-down" className="btn-like btn-like--down" onClick={() => setDownLike(downLike + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">{downLike}</span>
-        </div>
-      </div>
-
+      {jokes.map((item) => (
+        <Joke
+          key={item.id}
+          userAvatar={item.avatar}
+          userName={item.name}
+          text={item.text}
+          likes={item.likes}
+          dislikes={item.dislikes}
+        />
+      ))}
     </div>
-  );
+  )
 };
 
 
-createRoot(
-  document.querySelector('#app')
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
